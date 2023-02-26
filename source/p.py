1d6b9e9ea6d0d96adb983df41ee3971a26a4c583
Ali Uysal <aviator172rr@Alis-MacBook-Pro.local>
2023-02-25 21:55:00 -0800

    first draft

--- source/p.py
+++ source/p.py
@@ -6,7 +6,7 @@ from geopy.distance import distance
 geolocator = Nominatim(user_agent="distance_calculator")
 
 # put this csv file on a data frame
-df = pd.read_csv('/Users/aviator172rr/Desktop/planes/input/fuel_efficiency_4.csv') 
+df = pd.read_csv('/Users/aviator172rr/Desktop/planes/input/fuel_efficiency.csv') 
 
 
 dep = input("Enter departure city: ") # Ask user to enter a departure city
@@ -28,9 +28,14 @@ else:
 dist_km = dist * 1.852
 print(dist_km)
 
+# Still working on the next few lines. Trying to calculate the price of fuel burnt and display it on the plot.
 
+# filtered_fps = df.iloc[:, -1].str.slice(0, 4) 
+# tr = (dist_km * filtered_fps.astype(float))/100
+# gas_price = 4 # Price of fuel per liter in dollars
+# total_price = tr*gas_price # Total fuel price
+# print(total_price)
 
-filtered_fps = df.iloc[:, -1].str.slice(0, 4) 
 
 # In the bottom line, we take the first 4 characters of the sector column.
 # During this process, we get rid of the ',' (by using str.replace) in order to return this value as an integer. 
@@ -43,18 +48,13 @@ new_list = df[(df['Seats'] > capacity) & (real_sector > dist)]
 print(new_list.iloc[:, 0])
 
 
-
-
-
-
-
-
-plt.bar(new_list.iloc[:,0], new_list.iloc[:,2])
+plt.bar(new_list.iloc[:,0], new_list.iloc[:,-1])
 plt.xticks(rotation=90, fontsize = 9)
 plt.xlabel('Aircraft Model', labelpad=10)
-plt.ylabel('Flight Range (nm)')
+plt.ylim(bottom=0)
+plt.ylabel('Fuel Per Seat')
 plt.title('Aircraft List for this trip')
 fig = plt.gcf()
 fig.set_size_inches(10, 6)
-plt.subplots_adjust(bottom=0.35)
+plt.subplots_adjust(bottom=0.35, left=0.35)
 plt.show()
